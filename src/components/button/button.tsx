00e97f63e7b9415b1b2c0a6b44f089c3f9ff2262
Lurickardo <luizr726@gmail.com>
2024-04-28 17:38:08 -0300

    fix: add form example

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,11 +7,15 @@ interface ButtonProps {
   children: any;
   style?: string;
   loadingStyle?: string;
+  submitting?: boolean;
 }
 
-export function Button({ children, style, loadingStyle }: ButtonProps) {
-  const { pending } = useFormStatus();
-
+export function Button({
+  children,
+  style,
+  loadingStyle,
+  submitting,
+}: ButtonProps) {
   return (
     <button
       type="submit"
@@ -20,9 +24,9 @@ export function Button({ children, style, loadingStyle }: ButtonProps) {
           ? style
           : `bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`
       }
-      disabled={pending}
+      disabled={submitting}
     >
-      {pending ? <Spinner style={loadingStyle} /> : children}
+      {submitting ? <Spinner style={loadingStyle} /> : children}
     </button>
   );
 }
--- src/components/pages/noAuth/signin/form.tsx
+++ src/components/pages/noAuth/signin/form.tsx
@@ -1,30 +1,60 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/button/button";
-import { redirect } from "next/navigation";
+import { z } from "zod";
 
-export async function SignInForm() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  async function signIn(form: FormData) {
-    "use server";
+const signInFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, "Digite um email válido.")
+    .max(256, "Digite um email válido.")
+    .email("Digite um email válido."),
+  password: z.string().min(8),
+});
+
+type SignInForm = z.infer<typeof signInFormSchema>;
+
+export function SignInForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInForm>({
+    mode: "onBlur",
+    resolver: zodResolver(signInFormSchema),
+  });
+  const router = useRouter();
+
+  const signIn = async ({ email, password }: SignInForm) => {
+    console.log(email, password);
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    redirect("/dashboard");
-  }
+    router.push("/dashboard");
+  };
 
   return (
-    <form action={signIn} method="POST">
+    <form onSubmit={handleSubmit(signIn)} id="signInForm">
       <input
+        {...register("email")}
         type="email"
         name="email"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        required
       />
+      {errors.email && (
+        <span className="text-red-500 text-sm">{errors.email.message}</span>
+      )}
       <input
+        {...register("password")}
         type="password"
         name="password"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        required
       />
-      <Button>Sign In</Button>
+      {errors.password && (
+        <span className="text-red-500 text-sm">{errors.password.message}</span>
+      )}
+      <Button submitting={isSubmitting}>Sign In</Button>
     </form>
   );
 }
